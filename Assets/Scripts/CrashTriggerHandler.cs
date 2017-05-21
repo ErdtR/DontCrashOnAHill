2de30a2642bab037f61690099439c6655d490c35
Roman Erdt <romanerdt@gmail.com>
2017-05-21 18:15:33 +0300

    Add new scripts to project

---
+++ Assets/Scripts/CrashTriggerHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrashTriggerHandler : MonoBehaviour
+{
+    private bool isCrashed = false;
+
+    void OnTriggerEnter2D(Collider2D trigger)
+    {
+        if(trigger.gameObject.tag == "Trace")
+        {
+            isCrashed = true;
+        }
+    }
+
+    public bool IsCrashed()
+    {
+        return isCrashed;
+    }
+}

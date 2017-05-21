5593a79a39ee2a9664d157889c451a6dc1699d0b
Roman Erdt <romanerdt@gmail.com>
2017-05-21 20:43:50 +0300

    Add background music to game

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    private static SoundManager instance = null;
+
+    public static SoundManager Instance { get {return instance; } }
+
+	void Awake()
+    {
+        if(!instance)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
